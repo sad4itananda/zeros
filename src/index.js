a8c6418157c77d051c574b0ff52e77bc3d734d2d
sad4itananda <4ipej6ij@mail.ru>
2018-09-27 14:49:34 +0300

    refractoring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+	let count = 0;
+
+	const fivesDetector = (num) => {
+		if (num % 5 == 0) {count++};
+		num = num / 5;
+		if (num % 5 == 0 ) {fivesDetector(num)};
+	};
+
+	for (let i = 5; i <= number; i += 5) {
+		fivesDetector(i);
+	}
+	return count;
+};
